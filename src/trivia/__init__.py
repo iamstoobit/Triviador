8e529f434799a63a584208cd84cb39b6397d9afc
iamstoobit <martikanchev1@gmail.com>
2026-01-29 18:38:12 +0200

    Added database and management with mock data

---
+++ src/trivia/__init__.py
@@ -0,0 +1,5 @@
+from src.trivia.question import Question, QuestionType
+from src.trivia.database import TriviaDatabase
+from src.trivia.category_manager import CategoryManager
+
+__all__ = ['Question', 'QuestionType', 'TriviaDatabase', 'CategoryManager']
